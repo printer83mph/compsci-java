21b5b338bce912d9a874a49ca822b26b4770249e
printer83mph <printer.83mph@gmail.com>
2018-10-08 22:21:32 -0400

    Rename stuff

--- CompSci/src/parkingGarage/Car.java
+++ CompSci/src/parkingGarage/Car.java
@@ -1,4 +1,4 @@
-package classes;
+package parkingGarage;
 
 public class Car {
 
--- CompSci/src/parkingGarage/ParkingGarageTest.java
+++ CompSci/src/parkingGarage/ParkingGarageTest.java
@@ -1,4 +1,4 @@
-package classes;
+package parkingGarage;
 
 public class ParkingGarageTest {
 	public static void main(String[] args) {
